9dba7e64eff2aaabad0b06a07813b65182ba3ad5
Keerthick2309 <rithikj2300@gmail.com>
2026-03-22 19:53:56 +0530

    Changes

--- pages/Webcam Detection.py
+++ pages/Webcam Detection.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cv2
 from ultralytics import YOLO
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, RTCConfiguration
 import av
 
 st.title("Live Webcam Object Detection (YOLOv8)")
@@ -12,18 +12,27 @@ def load_model():
 
 model = load_model()
 
+RTC_CONFIGURATION = RTCConfiguration(
+    {
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+        ]
+    }
+)
+
 class VideoProcessor(VideoProcessorBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
         results = model(img)
-        annoated_frame = results[0].plot()
-        return annoated_frame
-    
+        annotated_frame = results[0].plot()
+        return annotated_frame
+
 st.write("Click start to begin webcam detection")
 
 webrtc_streamer(
     key="yolo-webcam",
     video_processor_factory=VideoProcessor,
     media_stream_constraints={"video": True, "audio": False},
+    rtc_configuration=RTC_CONFIGURATION,
     async_processing=True
 )
